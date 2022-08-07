cadf1be2d56f39734c9abfdd526393f9b3902431
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-07 19:38:31 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,23 +6,13 @@ import PhonebookList from './Phonebook/PhonebookList';
 import PhonebookFilter from './Phonebook/PhonebookFilter';
 
 export function App() {
-  const array = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-
-  const [contacts, setContacts] = useState(array);
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('storageContacts'))
+  );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    const parsedContacts = JSON.parse(localStorage.getItem('storageContacts'));
-    setContacts(parsedContacts);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('storageContacts', JSON.stringify(contacts));
+    window.localStorage.setItem('storageContacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const onSubmitForm = e => {
