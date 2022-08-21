3132ef2afe059483dfa5be661d5de2e8d3dfbe30
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-22 01:46:33 +0200

    AuthOperations

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,17 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import authOperations from './authOperations';
 
 const initialState = {
-    user: { name: null, email: null },
-    token: null,
-    isLoggedIn: false
-}
+  user: { name: null, email: null, password: null },
+  token: null,
+  isLoggedIn: false,
+};
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    extraReducers: {
-
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    [authOperations.register.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.password = action.payload.password;
+      state.isLoggedIn = true;
     },
-})
+    [authOperations.logIn.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.password = action.payload.password;
+      state.isLoggedIn = true;
+    },
+    [authOperations.logOut.fulfilled](state) {
+      state = initialState;
+    },
+  },
+});
 
 export default authSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsApi } from "./services/contacts";
 import filterReducers from "./contactsListReducers/filterReducers";
+import authSlice from "./auth/authSlice";
 
 export const store = configureStore({
     reducer: {
     [contactsApi.reducerPath]: contactsApi.reducer,
-      filter: filterReducers
+    filter: filterReducers,
+      auth: authSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(contactsApi.middleware)
 })
