295470defbaf4a9d9f78c4b46a8ce3a74f847302
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-23 14:50:59 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import AppBar from './AppBar';
 import RegisterView from './Authorizaton/RegisterView';
 import LogInView from './Authorizaton/LogInView';
 import ContactsPage from 'pages/ContactsPage';
 import authOperations from 'redux/auth/authOperations';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import authSelectors from 'redux/auth/authSelectors';
 
 export function App() {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
@@ -24,10 +26,20 @@ export function App() {
       }}
     >
       <AppBar />
+
       <Routes>
-        <Route path="/contacts" element={<ContactsPage />} />
-        <Route path="/register" element={<RegisterView />} />
-        <Route path="/login" element={<LogInView />} />
+        <Route
+          path="/contacts"
+          element={!isLoggedIn ? <Navigate to="/login" /> : <ContactsPage />}
+        />
+        <Route
+          path="/register"
+          element={isLoggedIn ? <Navigate to="/contacts" /> : <RegisterView />}
+        />
+        <Route
+          path="/login"
+          element={isLoggedIn ? <Navigate to="/contacts" /> : <LogInView />}
+        />
       </Routes>
     </div>
   );
--- src/components/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Route } from 'react-router-dom';
-import authSelectors from 'redux/auth/authSelectors';
-
-export default function PrivateRoute() {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-}
