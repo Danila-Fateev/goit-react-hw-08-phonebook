cb57383460d5cc23865a4eafc4fa401df5556829
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-24 14:25:49 +0200

    Notiflix

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import Notiflix from 'notiflix';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
@@ -18,7 +19,7 @@ const register = createAsyncThunk('auth/register', async credentials => {
     token.set(data.token);
     return data;
   } catch (error) {
-    alert(error.message);
+    Notiflix.Notify.failure(error.message);
     console.log(error);
   }
 });
@@ -29,7 +30,7 @@ const logIn = createAsyncThunk('auth/login', async credentials => {
     token.set(data.token);
     return data;
   } catch (error) {
-    alert(error.message);
+    Notiflix.Notify.failure(error.message);
     console.log(error);
   }
 });
